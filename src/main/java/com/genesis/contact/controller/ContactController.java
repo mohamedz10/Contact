1675d1d035f9138df3c3da68ce3550bea2639090
Mohamed <mohamedz.1@hotmail.com>
2021-06-27 21:00:06 +0200

    Add swagger ui

--- src/main/java/com/genesis/contact/controller/ContactController.java
+++ src/main/java/com/genesis/contact/controller/ContactController.java
@@ -2,12 +2,14 @@ package com.genesis.contact.controller;
 
 import com.genesis.contact.model.Contact;
 import com.genesis.contact.service.ContactService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@ApiOperation(value = "WEB API d’une gestion de contacts dans une entreprise.")
 @RequestMapping("/contact")
 public class ContactController {
 
